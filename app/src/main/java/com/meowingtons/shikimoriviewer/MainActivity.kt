36d96708e13308709de595e0667f47f73c15d83d
SirMeowingtons5 <tarannikov.va+github@gmail.com>
2018-08-03 02:57:37 +0300

    Added navigation between app and login

--- app/src/main/java/com/meowingtons/shikimoriviewer/MainActivity.kt
+++ app/src/main/java/com/meowingtons/shikimoriviewer/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.meowingtons.shikimoriviewer
 
+import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            val uriIntent = Uri.parse("app://com.meowingtons.shikimoriviewer/login")
+            //val uriIntent=Uri.parse("http://google.com")
+            val intent = Intent(Intent.ACTION_MAIN)
+
+            intent.data = uriIntent
+            startActivity(intent)
+        }
     }
 }
